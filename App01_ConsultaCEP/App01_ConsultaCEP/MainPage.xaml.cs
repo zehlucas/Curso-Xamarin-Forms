3264814e0e3153903e1667cc04bb1c09b01ac43d
José Lucas Brandão Montes <zehlucas@LIVE.COM>
2020-02-11 16:40:39 -0300

    Commit Inicial

---
+++ App01_ConsultaCEP/App01_ConsultaCEP/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_ConsultaCEP.Servico;
+using App01_ConsultaCEP.Servico.Modelo;
+
+namespace App01_ConsultaCEP
+{
+    [DesignTimeVisible(false)]
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Botao.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            //TODO - Lógica do Programa
+
+            //TODO - Validações
+
+            string cep = Cep.Text.Trim();
+
+            if (isValidCep(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+                    if(end != null)
+                    {
+                        Resultado.Text =
+                        string.Format(
+                            "Endereço:" +
+                            "CEP: {0}, \n" +
+                            "UF: {1}, \n" +
+                            "Logradouro: {2}\n" +
+                            "Bairro: {3}\n" +
+                            "Localidade: {4}\n",
+                            cep,
+                            end.uf,
+                            end.logradouro,
+                            end.bairro,
+                            end.localidade);
+                    }
+                    else
+                    {
+                        DisplayAlert("Erro!", "CEP Não encontrado: " + cep, "OK");
+                    }
+                }
+                catch(Exception e)
+                {
+                    DisplayAlert("Erro Crítico!", e.Message, "OK");
+                }
+            } 
+        }
+
+        private bool isValidCep(string cep)
+        {
+            bool valido = true;
+            if(cep.Length != 8)
+            {
+                //erro
+                DisplayAlert("Erro!", "Cep Inválido! O Cep deve conter 8 caracteres", "ok");
+                valido = false;
+            }
+            
+            int NovoCEP = 0;
+
+            if (!int.TryParse(cep, out NovoCEP))
+            {
+                //erro
+                DisplayAlert("Erro!", "Cep Inválido! O Cep deve conter apenas números", "ok");
+                valido = false;
+            }
+
+            return valido;
+        }
+    }
+}
---
+++ App01_ConsultaCEP/App01_ConsultaCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultaCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace App01_ConsultaCEP.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string novoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(novoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            if (end.cep == null) return null;
+
+            return end;
+        }
+    }
+}
